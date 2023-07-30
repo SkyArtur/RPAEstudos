57f3f92f6d1f6b0afb1f570677b29e5244f46848
Artur dos Santos Shon <sky_artur@hotmail.com>
2023-07-29 22:14:21 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from robot import DolarRobot
 import json
 
 if __name__ == '__main__':
-    file_password = Path(__file__).resolve().parent.joinpath('files/user_data.json')
     url_t = 'https://melhorcambio.com/dolar-hoje'
     element_t = '//*[@id="comercial"]'
-    with open(file_password, encoding='utf8') as file:
-        data_user = json.load(file)
+    sender = 'sender@email.com'
+    sender_password = 'password_email_sender'
     receiver = 'email@email.com'
     robot = DolarRobot(url_t, element_t)
     robot.execute_robot()
-    robot.sent_mail(data_user['email'], receiver, data_user['password'])
+    robot.sent_mail(sender, receiver, sender_password)
