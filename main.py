37140d6752967119515dfa32af8b65f43ac5151d
Artur dos Santos Shon <sky_artur@hotmail.com>
2023-07-29 22:21:17 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from robot import DolarRobot
 
 if __name__ == '__main__':
-    url_t = 'https://melhorcambio.com/dolar-hoje'
-    element_t = '//*[@id="comercial"]'
     sender = 'sender@email.com'
     sender_password = 'password_email_sender'
     receiver = 'email@email.com'
-    robot = DolarRobot(url_t, element_t)
+    robot = DolarRobot(url='https://melhorcambio.com/dolar-hoje', element='//*[@id="comercial"]')
     robot.execute_robot()
     robot.sent_mail(sender, receiver, sender_password)
